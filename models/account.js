06d9364bbe60e7cbbe0bec924723fac01f97937d
bluejoyq <bluejoyq@naver.com>
2020-10-13 15:24:09 +0000

    building new code

--- models/account.js
+++ models/account.js
@@ -1,35 +1,34 @@
-const mysql = require("mysql");
-
-var connection = mysql.createConnection({
-	host : "localhost",
-	user : "osam_tuna",
-	password : "pass_osam_tuna",
-	database : "osam_tuna"
-});
+import mysql from 'mysql';
+import {userIdValid, userPWValid, userNameValid, userRankValid, userTelValid, userUnitValid} from "../utils/validation.js";
+import sqlSetting from '../config/sqlSetting.js';
+const connection = mysql.createConnection(sqlSetting);
 
 //return : id or undefined;
-function isIdExists(id) {
-	var userId;
-	connection.connect();
-	connection.query("SELECT * from accounts where userId="+id, function(err, results, fields) {
-	if(err)
-		throw err;
-	userId = results[0].userId;	
-}); 
-	connection.end(); 
-	return userId;
+export function isIdExists(id) {
+	connection.query("SELECT * from account where userId="+id, function(err, results, fields) {
+		if(err)
+			throw err;
+		return results.length === 0;
+	}); 
+}
+
+//test function
+export function getAll() {
+	connection.query("SELECT * FROM account", function(err, results, fields) {
+		if(err)
+			throw err;
+		return results;
+	}); 
 }
 
 //return : id or undefined;
-function canLogin(id, hashed_pw) {
+export function canLogin(id, hashed_pw) {
 	if(!isIdExists(id)) 
 		return undefined;
-	
-	connection.connect();
-	connection.query("SELECT * FROM accounts WHERE userId="+id, function(err, results, fields) {
+	connection.query("SELECT * FROM account WHERE userId="+id, function(err, results, fields) {
 			if(err)
 				throw err;
-			connection.query("SELECT * FROM accounts WHERE userId="+id+" AND userPW="+hashed_pw+";",
+			connection.query("SELECT * FROM account WHERE userId="+id+" AND userPW="+hashed_pw+";",
 			function(err, results, fields) {
 				if(err)
 					throw err;
@@ -37,50 +36,31 @@ function canLogin(id, hashed_pw) {
 
 			});
 		});
-	connection.end();
 	return id;
 	
 };
 
 //must use when logined
 //return : userInfo
-function getuserInfo(id) {
+export function getuserInfo(id) {
 	var userInfo;
-	connection.connect();
-	connection.query("SELECT * from accounts where userId="+id+";", function(err, results, fields) {
+	connection.query("SELECT * FROM account WHERE userId="+id+";", function(err, results, fields) {
 		if(err)
 			throw err;
-		connection.query("SELECT * FROM accounts WHERE userId="+id+";", function(err, results, fields) {
-			if(err)
-				throw err;
-			userInfo = results[0];
-		});
+		userInfo = results[0];
+	});
 
 		//userInfo가 undefined 될 경우는 오직 db오류임.
-		if(!userInfo)
-			throw err;
-
-		return userInfo;
+	if(!userInfo)
+		throw err;
+	return userInfo;
 }
 
-function signUp(userInfo) {
-	const validation = require("../utils/validation.js");
-	if(validation.userIdValid(userInfo.userId))
-		return { wrongVal : "userId" }
-	if(validation.userPWValid(userInfo.userPW))
-		return { wrongVal : "userPW" }
-	if(validation.userNameValid(userInfo.userName))
-		return { wrongVal : "userName" }
-	if(validation.userRankValid(userInfo.userRank))
-		return { wrongVal : "userRank" }
-	if(validation.userTelValid(userInfo.userTel))
-		return { wrongVal : "userTel" }
-	if(validation.userUnitValid(userInfo.userUnit))
-		return { wrongVal : "userUnit" }
-
-	var values = "values("+userInfo.userId+","+userInfo.userPW+","+userInfo.UserName+","+userInfo.userTel+","+userInfo.userTel+","+userInfo.userRank+","+userInfo.user+","+userInfo.userUnit+")";
-	connection.connect();
-	connection.query("INSERT INTO accounts "+values+";", function(err, results, fields)	{
+export function signUp(userInfo) {
+	console.log("여기오냐ㅐㄴㅁ")
+	let tf = userInfo.isWorker ? 'TRUE':'FALSE';
+	let values = `values(${userInfo.userId},${userInfo.userName},${userInfo.userTel},${userInfo.userUnit},${userInfo.userRank},${userInfo.userPWHash},${userInfo.userPWSalt},${tf})`
+	connection.query("INSERT INTO account "+values+";", function(err, results, fields)	{
 		if(err)
 			throw err;
 		console.log("insert userInfo : "+values+" successfully");
@@ -90,11 +70,5 @@ function signUp(userInfo) {
 }
 
 
-module.export = {
-	isIdExists = isIdExists;
-	canLogin = canLogin;
-	getuserInfo = getuserInfo;		
-	signUp = signUp;
-	
-};
+
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
-var express = require("express");
+import express from "express";
+import {canLogin, getuserInfo, isIdExists, signUp, getAll} from "../models/account.js";
+import {userIdValid, userPWValid, userNameValid, userRankValid, userTelValid, userUnitValid} from "../utils/validation.js";
+import bkfd2Password from 'pbkdf2-password';
+import sqlSetting from '../config/sqlSetting.js';
+import mysql from 'mysql';
+const connection = mysql.createConnection(sqlSetting);
+connection.connect(function(err) {
+	if(err) console.error('mysql connection error : ' + err);
+	else console.log('mysql is connected successfully!');
+});
+const hasher = bkfd2Password();
+
 var app = express();
 var router = express.Router();
-var account = require("../models/account.js");
 
 
 
@@ -11,8 +22,8 @@ var account = require("../models/account.js");
 router.route("/login").post(function(req,res) {
 	console.log("POSTED userId , userPW");
 
-	if(account.canLogin(req.body.userId, req.body.userPW)) {
-		var userInfo = account.getuserInfo(req.body.userId);
+	if(canLogin(req.body.userId, req.body.userPW)) {
+		var userInfo = getuserInfo(req.body.userId);
 		//TODO 추후 수정 해야함?
 		req.session.user = userInfo;
 		req.session.user.authroized = true;
@@ -27,9 +38,14 @@ router.route("/login").post(function(req,res) {
 			code: 0
 		});
 	
+});
 
-		
-
+router.route("/test").get( async (req,res) =>{
+	let temp = await isIdExists('12-211');
+	console.log(temp)
+	res.status(200).json({
+		data: temp
+	});
 });
 
 
@@ -54,97 +70,88 @@ router.route("/logout").post(function(req, res) {
 	}
 });
 
-//미검증
-//회원가입
-router.route("/signup").post(function(req, res) {
-	
-	//ㅇㅖ시, 나중에 지우면 댐
-	var userInfo_example = {
-		userId: "string",
-		userPW: "string",
-		userName: "string",
-		userTel: "string",
-		userRank: "string",
-		userUnit: "string"
-	};
-	
 
-
-   // 아이디 유효성 검사
-	if(validation.userIdValid(userInfo.userId)) {
+function validAll (req, res, next){
+	let userInfo = {...req.body.userInfo};
+	if(!userIdValid(userInfo.userId)) {
 		return res.status(400).json({
-			error: "Bad userId",
+			type: "Bad userId",
 			code: 1
 		});
 	}
-	
-	//비밀번호 유효성 검사
-    if(validation.userPWValid(userInfo.userPW) {
-    	return res.status(400).json({
-			error: "Bad userPW",
+	else if(!userPWValid(userInfo.userPW)) {
+		return res.status(400).json({
+			type: "Bad userPW",
 			code: 2
 		});
 	}
-    
-	//사용자 이름 유효성 검사
-    if(validation.userNameValid(userInfo.userName) {
-    	return res.status(400).json({
-			error: "Bad userName",
+	else if(!userNameValid(userInfo.userName)) {
+		return res.status(400).json({
+			type: "Bad userName",
 			code: 3
 		});
 	}
-
-   	//사용자 ㄱㅖ급 유효성 검사
-    if(validation.userRankValid(userInfo.userRank) {
-    	return res.status(400).json({
-			error: "Bad userRank",
+	else if(!userRankValid(userInfo.userRank)) {
+		return res.status(400).json({
+			type: "Bad userRank",
 			code: 4
 		});
 	}
-       
-   	//사용자 전화번호 유효성 검사
-    if(validation.userTelValid(userInfo.userTel) {
-    	return res.status(400).json({
-			error: "Bad userTel",
+	else if(!userTelValid(userInfo.userTel)) {
+		return res.status(400).json({
+			type: "Bad userTel",
 			code: 5
 		});
 	}
-
-	//사용자 소속 유효성 검사
-    if(validation.userPWValid(userInfo.userPW) {
-    	return res.status(400).json({
-			error: "Bad userUnit",
+	else if(!userPWValid(userInfo.userPW)) {
+		return res.status(400).json({
+			type: "Bad userUnit",
 			code: 6
 		});
 	}
-    
-     
+	connection.query("SELECT * from account where userId="+userInfo.userId, function(err, results, fields) {
+		if(err)
+			res.status(400).json({
+				type: "err",
+				code: 10
+			});
+		if(results.length === 0) next();
+		else {
+			res.status(400).json({
+				type: "Id Already Exist",
+				code: 10
+			});
+		}
+	}); 
+}
 
-	if(!account.isIdExists(req.body.userId))
-		hasher({password:req.body.userPW}, function(err, pass, salt, hash) {
-			var userInfo = {
-				userId = req.body.userId,
-				userPW = pass,
-				userName = req.body.userName,
-				userTel = req.body.userTel,
-				userRank = req.body.userRank,
-				userUnit = req.body.userUnit
+router.route("/signup").post(validAll,(req, res)=> {
+	let tempInfo = {...req.body.userInfo};
+	hasher({password:tempInfo.userPW}, async function(err, pass, salt, hash) {
+		delete tempInfo['userPW'];
+		let userInfo =  {
+			...tempInfo,
+			userPWSalt : salt,
+			userPWHash : hash,
+		};
+		let tf = userInfo.isWorker ? 'TRUE':'FALSE';
+		let values = `values('${userInfo.userId}','${userInfo.userName}','${userInfo.userTel}','${userInfo.userUnit}','${userInfo.userRank}','${userInfo.userPWHash}','${userInfo.userPWSalt}',${tf})`
+		connection.query("INSERT INTO account "+values+";", function(err, results, fields)	{
+			if(err)
+				res.json({success:false, error: err, type: "sql error", code: 11});
+			else {
+				res.json({success:true});
 			}
-			account.signUp(userInfo);
-			res.json({success:true}); 
-		});
-	else
-		res.status(400).json({
-			error: "Id already Exists",
-			code: 10
-		});
+			
+		})
+	})
 });
 
 
 //미검증
 //userInfo pw제외 send
 router.route("/info").post(function(req,res) {
-	var userInfo = account.getUserInfo(req.body.userId);
+	var userInfo = getUserInfo(req.body.userId);
 	if(!userInfo)
 		res.status(400).json({success:false});
 	
@@ -152,7 +159,5 @@ router.route("/info").post(function(req,res) {
 	res.json(userInfo);	
 });	
 
-module.exports = router;
-
-
+export default router;
 
